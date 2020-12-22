8b5bd580f0f1e172d567a56239346d2222525b55
xnikoloff <59517219+xnikoloff@users.noreply.github.com>
2020-12-22 15:35:16 +0200

    Update BrandService.cs

--- TheUndergroundConsole.Services/BrandService.cs
+++ TheUndergroundConsole.Services/BrandService.cs
@@ -16,7 +16,7 @@ namespace TheUndergroundConsole.Services
             this.dbContext = dbContext;
         }
 
-
+        //Initial data import to the Db
         public void ImportData(string name)
         {
             if(name == "" || name == null)
@@ -32,7 +32,9 @@ namespace TheUndergroundConsole.Services
             this.dbContext.Brands.Add(property);
             this.dbContext.SaveChanges();
         }
-
+        
+        //Used in the initial data import to select a brand as a foreign key for the car models 
+        //belonging to the given brand
         public Brand SelectBrand()
         {
             var brands = this.dbContext.Brands
@@ -46,10 +48,12 @@ namespace TheUndergroundConsole.Services
             {
                 Console.WriteLine($"{brand.Id}. {brand.Name}");
             }
-
+            
+            //select the id of the brand you want to return
             Console.Write("Select brand's id: ");
             int brandId = int.Parse(Console.ReadLine());
-
+            
+            //get the desired brand from the Db
             var selectedBrand = this.dbContext.Brands
                 .First(b => b.Id == brandId);
 
